d8e5df969e9273ceefa7997cac327253f3d62d04
Zack Zellto <zacharyzellto@gmail.com>
2023-04-30 19:02:39 -0600

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-500 underline text-center">
+        React App
+      </h1>
+    </>
+  );
+}
+
+export default App;
